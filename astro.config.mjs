68caca7faf6ce8ae78d343282c03197ca034c163
Taha Rashid <taharashid710192@gmail.com>
2026-06-25 22:19:15 -0400

    Installed starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
 // See https://docs.astro.build/en/guides/deploy/github/
 export default defineConfig({
-    site: 'https://p3dual.com',
+  site: 'https://p3dual.com',
+  integrations: [starlight({
+    title: 'P3Dual Project Docs',
+  }),],
 });

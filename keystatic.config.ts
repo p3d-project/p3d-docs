a0c329a0979727a8ff919fe657ae9ff7adc8aa16
agatemosu <agaatem@outlook.com>
2026-07-16 13:47:29 +0200

    update keystatic to have collections

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,53 +1,129 @@
-import { config, fields, collection } from '@keystatic/core';
-import { block } from '@keystatic/core/content-components';
+import {
+  collection,
+  config,
+  fields,
+  singleton,
+  type ComponentSchema,
+} from '@keystatic/core';
+import {
+  block,
+  type ContentComponent,
+} from '@keystatic/core/content-components';
+
+const baseComponents = {
+  GoogleDoc: block({
+    label: 'Google Document',
+    schema: {
+      src: fields.url({ label: 'Source' }),
+      isEditable: fields.checkbox({
+        label: 'Is Editable',
+        defaultValue: true,
+      }),
+    },
+  }),
+  RemoteMd: block({
+    label: 'Remote Markdown',
+    schema: {
+      src: fields.url({ label: 'Source' }),
+    },
+  }),
+} satisfies Record<string, ContentComponent>;
+
+const baseSchema = {
+  tableOfContents: fields.checkbox({
+    label: 'Table of Contents',
+    defaultValue: true,
+  }),
+  content: fields.markdoc({
+    label: 'Content',
+    components: baseComponents,
+    options: {
+      image: {
+        directory: 'src/assets/docs/images',
+        publicPath: '@assets/docs/images',
+      },
+    },
+  }),
+} satisfies Record<string, ComponentSchema>;
+
+const singletonSchema = {
+  title: fields.text({
+    label: 'Title',
+  }),
+  ...baseSchema,
+} satisfies Record<string, ComponentSchema>;
+
+const collectionSchema = {
+  title: fields.slug({
+    name: { label: 'Title' },
+  }),
+  ...baseSchema,
+} satisfies Record<string, ComponentSchema>;
 
 export default config({
   storage: {
     kind: 'github',
-    repo: 'p3d-project/p3d-website',
+    repo: {
+      owner: 'p3d-project',
+      name: 'p3d-website',
+    },
+  },
+  singletons: {
+    roadmap: singleton({
+      label: 'Roadmap',
+      path: 'src/content/docs/docs/roadmap',
+      format: { contentField: 'content' },
+      schema: singletonSchema,
+    }),
+    credits: singleton({
+      label: 'Credits',
+      path: 'src/content/docs/docs/credits',
+      format: { contentField: 'content' },
+      schema: singletonSchema,
+    }),
   },
   collections: {
-    docs: collection({
-      label: 'Docs',
+    getting_started: collection({
+      label: 'Getting Started',
       slugField: 'title',
-      path: 'src/content/docs/docs/*',
+      path: 'src/content/docs/docs/getting-started/**',
       format: { contentField: 'content' },
-      schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        tableOfContents: fields.checkbox({
-          label: 'Table of Contents',
-          defaultValue: true,
-        }),
-        // TODO: add starlight options?
-        // https://github.com/withastro/starlight/blob/main/packages/starlight/schema.ts
-        content: fields.markdoc({
-          label: 'Content',
-          options: {
-            image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
-            },
-          },
-          components: {
-            GoogleDoc: block({
-              label: 'Google Doc',
-              schema: {
-                src: fields.url({ label: 'Source' }),
-                isEditable: fields.checkbox({
-                  label: 'Is Editable',
-                  defaultValue: true,
-                }),
-              },
-            }),
-            RemoteMd: block({
-              label: 'Remote Markdown',
-              schema: {
-                src: fields.url({ label: 'Source' }),
-              },
-            }),
-          },
-        }),
-      },
+      schema: collectionSchema,
+    }),
+    milestones: collection({
+      label: 'Milestones',
+      slugField: 'title',
+      path: 'src/content/docs/docs/milestones/**',
+      format: { contentField: 'content' },
+      schema: collectionSchema,
+    }),
+    p3d_amicitia: collection({
+      label: 'P3D-Amicitia',
+      slugField: 'title',
+      path: 'src/content/docs/docs/p3d-amicitia/**',
+      format: { contentField: 'content' },
+      schema: collectionSchema,
+    }),
+    p3d_game: collection({
+      label: 'P3D-Game',
+      slugField: 'title',
+      path: 'src/content/docs/docs/p3d-game/**',
+      format: { contentField: 'content' },
+      schema: collectionSchema,
+    }),
+    p3d_website: collection({
+      label: 'P3D-Website',
+      slugField: 'title',
+      path: 'src/content/docs/docs/p3d-website/**',
+      format: { contentField: 'content' },
+      schema: collectionSchema,
+    }),
+    references: collection({
+      label: 'References',
+      slugField: 'title',
+      path: 'src/content/docs/docs/references/**',
+      format: { contentField: 'content' },
+      schema: collectionSchema,
     }),
   },
 });
